21d6c97cfcee11729c0b43ad487fcc91029eed4b
amank55 <amankumarr2005ak@gmail.com>
2025-07-23 17:09:32 +0530

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,15 @@
-import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+const inter = Inter({ subsets: ["latin"] });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Plixx",
-  description: "An Ai Photo Generating App",
+export const metadata = {
+  title: "Pixxel",
+  description: "Professional image editing powered by AI",
 };
+
 
 export default function RootLayout({
   children,
@@ -24,9 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en"suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="icon" href="/logo-text.png" sizes="any" />
+      </head>
+      <body className={`${inter.className}`}>
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        />
         {children}
       </body>
     </html>
